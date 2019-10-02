fdfb2170ce4aab0afe790b2e957f97db4506766b
Sara Harvey <saraharveynyc@gmail.com>
2019-10-02 23:41:48 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if session[:user_id] == nil
+      erb :'/users/create_user'
+    else
+      redirect to '/tweets'
+    end
+  end
 
 end
